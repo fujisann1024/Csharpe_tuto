72ee8ab192f1405cb6886231bca2f52aa3ac7516
fujisann1024 <tehepero302@gmail.com>
2020-12-27 01:09:27 +0900

    test

---
+++ console/Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tutorial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //標準出力
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
