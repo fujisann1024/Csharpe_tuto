551c965db0dd8995b3777deb3b6a915ecee7d688
fujisann1024 <tehepero302@gmail.com>
2021-01-11 17:37:58 +0900

    class

--- console/Object_Program/Object1/Program.cs
+++ console/Object_Program/Object1/Program.cs
@@ -32,14 +32,93 @@ namespace Car
     /// </summary>
     class Car
     {
-        //メンバー変数
-        public double totalRunKm; ///走行距離(km)
-        public double gasL;       ///ガソリン量(L)
-        public double runKmH;     ///走行速度(km/h)
-        public double timeM;      ///走行時間(分)
+        ////メンバー変数
+        //public double totalRunKm; ///走行距離(km)
+        //public double gasL;       ///ガソリン量(L)
+        //public double runKmH;     ///走行速度(km/h)
+        //public double timeM;      ///走行時間(分)
+
+        //カプセル化を行ったメンバー変数
+        private double totalRunKm; ///走行距離(km)
+        private double gasL;       ///ガソリン量(L)
+        private double runKmH;     ///走行速度(km/h)
+        private double timeM;      ///走行時間(分)
+
+        //変数初期化子
+        public bool Fault = false; ///故障しているかどうか(falseなら正常)
+
+        //コンストラクター～newを用いてインスタンス化した際に呼び出されるメソッド
+
+        //引数なしコンストラクタ
+        //コンストラクター初期化子～あるコンストラクターから別のコンストラクターを呼び出す
+        //下の例の場合は引数付きコンストラクターを適用している
+        //変数初期化子⇒コンストラクター初期化子⇒コンストラクター本体の順番で実行
+
+        ///<summary>
+        ///
+        /// </summary>
+        public Car():this(100,0,0)
+        {
+
+            this.totalRunKm = 0;
+
+        }
+        //引数付きコンストラクタ
+        public Car(double gasL, double runKmH,double timeM)
+        {
+            this.gasL = gasL;
+            this.runKmH = runKmH;
+            this.timeM = timeM;
+        }
+
+        //アクセサー～メンバー変数の取得や変更を行うメソッド
+        //    ⇒privateにしたら設けるメソッド
+        /*
+         * C++やJavaの場合だとフィールドの数だけアクセサーが増える
+         * 利用者側からはフィールドに直接代入したほうが見栄えが良い
+         * ⇒C#ではクラス内部から見るとメソッドのように振る舞い、利用者側からみるとメンバー変数のようにふるまう機能
+         * ⇒プロパティ
+         * プロパティ名とフィールド名は違う書き方でないとエラーが起きる
+         * アクセスレベル 型名 プロパティ名{
+         *      get
+         *      set
+         * }
+         */
+        public double TotalRunKm
+        {
+            //ゲッター(getter)
+            get { return this.totalRunKm; }
+            //セッター(setter)
+            //アクセスレベルの設定Ver.2.0
+            private set { this.totalRunKm = value; }
+        }
+        public double GasL
+        {
+            //ゲッター(getter)
+            get { return this.gasL; }
+            //セッター(setter)
+            set { this.gasL = value; }
+        }
+
+        public double RunKmH
+        {
+            //ゲッター(getter)
+            get { return this.runKmH; }
+            //セッター(setter)
+             set { this.runKmH = value; }
+        }
+
+        public double TimeM
+        {
+            //ゲッター(getter)
+            get { return this.timeM; }
+            //セッター(setter)
+            set { this.timeM = value; }
+        }
 
 
         //メソッド
+
 
         ///<summary>
         ///走行する
@@ -65,7 +144,7 @@ namespace Car
         /// </summary>
         public void dispMeter()
         {
-            Console.WriteLine($"今の総合走行距離は{totalRunKm}kmです");
+            Console.WriteLine($"今の総合走行距離は{this.totalRunKm}kmです");
         }
 
         /// <summary>
@@ -102,8 +181,8 @@ namespace Car
             ⇒データに値を入れる必要がある
             変数名.メンバー名
             */
-            car.runKmH = 40; //時速
-            car.timeM = 100; //走行時間(分)
+            car.RunKmH = 40; //時速
+            car.TimeM = 100; //走行時間(分)
 
             /*
              インスタンスを操作する
--- console/Object_Program/Parts/Program.cs
+++ console/Object_Program/Parts/Program.cs
@@ -5,7 +5,7 @@ namespace Parts
     partial class Parts
     {
         public string Name;//タイヤ名
-        public bool Fault = false;      //故障状態
+        public bool Fault = false;      //故障状態f(falseならば正常)
 
 
 
@@ -14,20 +14,61 @@ namespace Parts
 
     partial class Parts
     {
+        /// <summary>
+        /// パーツの名前を表示
+        /// </summary>
         public void dispPartsName()
         {
             //thisは分割された変数フィールドを参照している
             Console.WriteLine($"このパーツの名前は{this.Name}です"); 
         }
 
+        /// <summary>
+        /// 故障してるかの表示
+        /// </summary>
         public void judgeFault()
         {
-            if (!this.Fault)
+            if (this.Fault)
             {
-                Console.WriteLine("このパーツは何らかの原因で故障しています");
+                Console.WriteLine($"{this.Name}は何らかの原因で故障しています");
                 return;
             }
-            Console.WriteLine("このパーツは正常です");
+            Console.WriteLine($"{this.Name}は正常です");
+        }
+
+        public static void Main()
+        {
+            Parts parts = new Parts();
+            parts.Name = "タイヤ";
+            parts.Fault = true;
+
+            parts.dispPartsName();
+            parts.judgeFault();
+
+            //匿名型Ver 3.0
+            var rectangle = new { width = 2, height = 3 };
+            /*
+             下記のコードと同じ
+             class a {
+                private int width;
+                private int height;
+
+                public __a(int width,int height){
+                    this.width = width;
+                    this.height =~height;
+                }
+
+                public int width{
+                    get { return this.width }
+                }
+
+                public int height{
+                    get { return this.height }
+                }
+            }
+             
+             */
+            Console.WriteLine($"幅:{rectangle.width},高さ:{rectangle.height}");
         }
 
     }
